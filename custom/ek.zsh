e6f68b5c9540cf088cf5cc766798eab62304961d
eyjolfur12 <ek@syn.is>
2022-09-19 16:34:04 +0000

    Add files via upload

---
+++ custom/ek.zsh
@@ -0,0 +1,6 @@
+alias la="ls -lshAG"
+alias ga="git add ."
+alias gs="git status"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
